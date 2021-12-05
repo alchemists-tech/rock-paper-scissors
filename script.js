17a63f99ec60dba08e3470f06b75b21f179c8e0f
spiknumb <spiknumb@netscape.net>
2021-12-05 13:27:33 -0500

    first attempt at writing the game script

--- script.js
+++ script.js
@@ -1,13 +1,99 @@
 const options = ['rock', 'paper', 'scissors'];
 
+let computerWins = 0;
+
+let userWins = 0;
+
+alert("Let's play Rock, Paper, Scissors!!");
+
+function computerPlay() {
+
+    return options[random()];
+
+}
+
+function getUserPlay() {
+
+    return prompt("Choose one: Rock, Paper, or Scissors..").toLowerCase();
+
+}
+
 function random() {
 
     return Math.floor(Math.random() * options.length);
 
 }
 
-function computerPlay() {
 
-    return options[random()]
+function game() {
+    
+    let computerChoice = computerPlay();
+    let userPlay = getUserPlay();
+    playRound(userPlay, computerChoice);
+
+    userPlay = getUserPlay();
+    computerChoice = computerPlay();
+    playRound(userPlay, computerChoice);
+
+    userPlay = getUserPlay();
+    computerChoice = computerPlay();
+    playRound(userPlay, computerChoice);
+
+    userPlay = getUserPlay();
+    computerChoice = computerPlay();
+    playRound(userPlay, computerChoice);
+
+    userPlay = getUserPlay();
+    computerChoice = computerPlay();
+    playRound(userPlay, computerChoice);
+
+    if (computerWins > userWins) {
+
+        alert("You lose, computer wins! Better luck next time!")
+
+    } else {
+
+        alert("You win! Computer loses!")
+
+    }
 
 }
+
+function playRound(userPlay, computerChoice) {
+
+    while (computerChoice === userPlay) {
+        userPlay = prompt("Its a tie!! Pick again: Rock, Paper, or Scissors");
+        computerChoice = computerPlay();
+    }
+
+    if (computerChoice === "rock" && userPlay === "scissors") {
+
+        computerWins++;
+
+    } else if (computerChoice === "paper" && userPlay === "rock") {
+
+        computerWins++;
+
+    } else if (computerChoice === "scissors" && userPlay === "paper") {
+
+        computerWins++;
+
+    } else if (computerChoice === "rock" && userPlay === "paper") {
+
+        userWins++;
+
+    } else if (computerChoice === "paper" && userPlay === "scissors") {
+
+        userWins++;
+
+    }  else if (computerChoice === "scissors" && userPlay === "rock") {
+
+        userWins++;
+
+    }
+
+}
+
+game();
+
+
